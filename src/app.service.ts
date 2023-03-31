7d7f742b7ce88f20f46e07665dafc4c5b79b7fff
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-03-30 23:56:53 -0300

    feat: add mock memoryUsage

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,12 +4,13 @@ export type HealthCheckResults = {
   dbStatus: string
   lastCronRun: Date
   uptime: number
+  memoryUsage: string
 }
 
 @Injectable()
 export class AppService {
   healthCheck(): HealthCheckResults {
-    // TODO switch dbStatus, lastCronRun, and uptime to use real data
-    return { dbStatus: 'ok', lastCronRun: new Date(), uptime: 10000 }
+    // TODO switch dbStatus, lastCronRun, uptime, and memoryUsage to use real data
+    return { dbStatus: 'ok', lastCronRun: new Date(), uptime: 10000, memoryUsage: '100MB' }
   }
 }
