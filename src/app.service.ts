c8d9fdbca2562d132e62bb3c7fc02f9b1b00e52f
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-03-30 23:52:38 -0300

    feat: add mock uptime

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,12 +3,13 @@ import { Injectable } from '@nestjs/common'
 export type HealthCheckResults = {
   dbStatus: string
   lastCronRun: Date
+  uptime: number
 }
 
 @Injectable()
 export class AppService {
   healthCheck(): HealthCheckResults {
-    // TODO switch dbStatus and lastCronRun to use real data
-    return { dbStatus: 'ok', lastCronRun: new Date() }
+    // TODO switch dbStatus, lastCronRun, and uptime to use real data
+    return { dbStatus: 'ok', lastCronRun: new Date(), uptime: 10000 }
   }
 }
