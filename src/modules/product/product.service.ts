7bba6f67ac5ad5c9fd2f33d0591e1a6da3cdac82
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-03-31 01:28:43 -0300

    feat: add pagination to findAll products

--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,26 +1,28 @@
-import { Injectable } from '@nestjs/common';
-import { CreateProductDto } from './dto/create-product.dto';
-import { UpdateProductDto } from './dto/update-product.dto';
+import { PaginationQuery, PaginationResponse } from '@libs/types/pagination'
+import { Injectable } from '@nestjs/common'
+import { CreateProductDto } from './dto/create-product.dto'
+import { UpdateProductDto } from './dto/update-product.dto'
 
 @Injectable()
 export class ProductService {
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    return 'This action adds a new product'
   }
 
-  findAll() {
-    return `This action returns all product`;
+  findAll(query: PaginationQuery): PaginationResponse<string> {
+    const result = `This action returns all product`
+    return new PaginationResponse(query, 1, [result])
   }
 
   findOne(id: number) {
-    return `This action returns a #${id} product`;
+    return `This action returns a #${id} product`
   }
 
   update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+    return `This action updates a #${id} product`
   }
 
   remove(id: number) {
-    return `This action removes a #${id} product`;
+    return `This action removes a #${id} product`
   }
 }
