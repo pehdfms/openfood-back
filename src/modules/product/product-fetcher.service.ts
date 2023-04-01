5095f4320944c886ae7ad893c704a300e605d947
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-03-31 22:50:45 -0300

    feat: add axios, repository and orm di to product fetcher

--- src/modules/product/product-fetcher.service.ts
+++ src/modules/product/product-fetcher.service.ts
@@ -1,15 +1,24 @@
-import { EntityRepository } from '@mikro-orm/core'
+import { EntityRepository, MikroORM, UseRequestContext, wrap } from '@mikro-orm/core'
 import { InjectRepository } from '@mikro-orm/nestjs'
 import { Injectable, Logger } from '@nestjs/common'
 import { Cron, CronExpression } from '@nestjs/schedule'
-import { Product } from './entities/product.entity'
+import { FetchStatus } from './entities/fetch-status.entity'
+import { Product, ProductStatus } from './entities/product.entity'
+import { HttpService } from '@nestjs/axios'
+import { existsSync, readFileSync, writeFileSync } from 'fs'
+import { gunzipSync } from 'zlib'
+import path from 'path'
 
 @Injectable()
 export class ProductFetcherService {
   private readonly logger = new Logger(ProductFetcherService.name)
 
   constructor(
-    @InjectRepository(Product) private readonly productRepository: EntityRepository<Product>
+    @InjectRepository(Product) private readonly productRepository: EntityRepository<Product>,
+    @InjectRepository(FetchStatus)
+    private readonly fetchStatusRepository: EntityRepository<FetchStatus>,
+    private readonly httpService: HttpService,
+    private readonly orm: MikroORM
   ) {}
 
   @Cron(CronExpression.EVERY_SECOND)
