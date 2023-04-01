ad77f09b488e420bf840a479006c0a292d65ba83
Pedro Henrique Garcia <pedro@projetodigital.com.br>
2023-04-01 14:56:27 -0300

    docs: improve swagger docs for sync controller

--- src/modules/sync/sync.controller.ts
+++ src/modules/sync/sync.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Post, Query } from '@nestjs/common'
-import { ApiPropertyOptional, ApiTags } from '@nestjs/swagger'
+import { ApiCreatedResponse, ApiOperation, ApiPropertyOptional, ApiTags } from '@nestjs/swagger'
 import { IsNumber, IsOptional, Min } from 'class-validator'
 import { ProductFetcherService } from './services/product-fetcher.service'
 
@@ -17,6 +17,14 @@ export class SyncController {
   constructor(private readonly productFetcherService: ProductFetcherService) {}
 
   @Post()
+  @ApiOperation({
+    description: 'Synchronizes database with upstream index',
+    summary: 'Synchronize DB with index'
+  })
+  @ApiCreatedResponse({
+    description: 'Synchronization ran succesfully',
+    schema: { example: 'Downloaded 100 products while synchronizing' }
+  })
   async sync(@Query() query: SyncQueryParams): Promise<string> {
     const downloadCount = await this.productFetcherService.fetch(query.count)
     return `Downloaded ${downloadCount} product${downloadCount > 1 ? 's' : ''} while synchronizing`
